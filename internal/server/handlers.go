5a07d76c2078193d131058a2d24da6d6e3320449
sinusxd <senya.kolyshkin.gg@gmail.com>
2024-12-05 00:16:25 +0300

    change cors

--- internal/server/handlers.go
+++ internal/server/handlers.go
@@ -37,8 +37,13 @@ func (s *Server) MapHandlers(e *echo.Echo) error {
 
 	mw := middleware2.NewMiddlewareManager(s.log, s.cfg)
 
+	allowedOrigins := "http://localhost:3000"
+	if s.cfg.App.Env == "prod" {
+		allowedOrigins = "http://localhost:80"
+	}
+
 	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{allowedOrigins},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		AllowMethods:     []string{http.MethodGet, http.MethodPost, http.MethodPut, http.MethodDelete},
 		AllowCredentials: true, // разрешает отправку учетных данных
